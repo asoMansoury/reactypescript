f3611f663483c05a490d1f0532cfe1086003ae53
unknown <aso.mansoury@gmail.com>
2023-12-04 23:12:46 -0500

    second publish

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,47 @@ import goalsImg from './assets/react.svg';
 import './App.css'
 import CourseGoal from './components/CourseGoal'
 import Header from './components/Header';
-
+type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+}
 function App() {
-  const [count, setCount] = useState(0)
+  // const [goals,setGoals] = useState<Array<CourseGoal>>([]);
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal() {
+    /// ...
+    setGoals(prevGoals => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        description: "hello",
+        title: "aso"
+      }
+      return [...prevGoals, newGoal]
+    });
+  }
+
 
   return (
     <main>
-      <Header image={{src:goalsImg,alt:'A List of goals'}}>
+      <Header image={{ src: goalsImg, alt: 'A List of goals' }}>
         <h1>Your Course Goals</h1>
       </Header>
-      <CourseGoal 
-        title="Learn React + TS" 
-        >
-          <p>Learn it from the ground up</p>
-        </CourseGoal>
+      <button onClick={handleAddGoal}>Add Goal</button>
+      <ul>
+        {
+          goals.map((item) => (
+            <li key={item.id}>
+              <CourseGoal
+                title={item.title}
+              >
+                <p>{item.description}</p>
+              </CourseGoal>
+            </li>
+          ))
+        }
+      </ul>
     </main>
   )
 }
