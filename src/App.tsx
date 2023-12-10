ed31fe43dfc45057bb0d2937685daab84547bf95
unknown <aso.mansoury@gmail.com>
2023-12-10 14:16:00 -0500

    add Container Props

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import './App.css'
 import Header from './components/Header';
 import CourseGoalList from './components/CourseGoalList';
 import NewGoal from './components/NewGoal';
+import Container from './components/BasicComponent/Container';
+import Button from './components/BasicComponent/Button';
+
 
 export type CourseGoalType = {
   title: string;
@@ -35,6 +38,9 @@ function App() {
 
   return (
     <main>
+      <Container as={Button}>hello</Container>
+
+      
       <Header image={{ src: goalsImg, alt: 'A List of goals' }}>
         <h1>Your Course Goals</h1>
       </Header>
